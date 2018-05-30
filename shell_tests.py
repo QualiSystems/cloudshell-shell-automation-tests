a66864a6888f67bd8b792302e49319c51b7ffba0
Kyrylo Maksymenko <saklar13@gmail.com>
2018-05-30 15:08:31 +0300

    fix typo

--- shell_tests.py
+++ shell_tests.py
@@ -14,8 +14,8 @@ def get_logger():
 
     handler = logging.StreamHandler()
 
-    formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formater)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
 
     logger.addHandler(handler)
 
