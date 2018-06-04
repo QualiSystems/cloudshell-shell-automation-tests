f6c3ca41d82aade62601a1b9cd3e4f498fd9fdad
Kyrylo Maksymenko <saklar13@gmail.com>
2018-06-04 12:12:32 +0300

    add ability to install script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+def get_file_content(file_path):
+    with open(file_path) as fp:
+        return fp.read()
+
+
+setup(
+    name='shell_tests',
+    url='http://www.qualisystem.com/',
+    author_email='info@qualisystems.com',
+    packages=find_packages(),
+    install_requires=get_file_content('requirements.txt'),
+    version=get_file_content('version.txt'),
+    description='QualiSystems automation tests for Shells',
+    include_package_data=True,
+    entry_points={'console_scripts': ['shell_tests = shell_tests.shell_tests:cli']},
+)
