709d29b7292e44954e4305b78101e707196d131b
Kyrylo Maksymenko <saklar13@gmail.com>
2018-06-05 14:52:45 +0300

    change a log level, send the log with a reporting email

--- shell_tests/cli.py
+++ shell_tests/cli.py
@@ -1,5 +1,6 @@
 import logging
 import traceback
+from StringIO import StringIO
 
 import click
 
@@ -14,19 +15,26 @@ def cli():
 
 
 def get_logger():
-    log_level = logging.INFO
+    log_level = logging.DEBUG
+    stream = StringIO()
 
     logger = logging.getLogger('Automation Tests')
     logger.setLevel(log_level)
 
-    handler = logging.StreamHandler()
+    handler = logging.StreamHandler(stream)
+    std_handler = logging.StreamHandler()
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
+    logger.addHandler(std_handler)
 
     return logger
+
+
+def get_log_msg(logger):
+    return logger.handlers[0].stream.getvalue()
 
 
 @cli.command()
@@ -41,13 +49,13 @@ def run_tests(config_path):
         if conf.report:
             error_msg = traceback.format_exc()
             smtp_client = SMTPClient(conf.report.user, conf.report.password, conf.report.recipients)
-            smtp_client.send_error(error_msg, conf.shell_path)
+            smtp_client.send_error(error_msg, conf.shell_path, get_log_msg(logger))
 
         raise
 
     if conf.report:
         smtp_client = SMTPClient(conf.report.user, conf.report.password, conf.report.recipients)
-        smtp_client.send_tests_result(is_success, result, conf.shell_path)
+        smtp_client.send_tests_result(is_success, result, conf.shell_path, get_log_msg(logger))
 
     return is_success, result
 
--- shell_tests/report_result.py
+++ shell_tests/report_result.py
@@ -1,7 +1,9 @@
 import re
 import smtplib
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email import Encoders
 
 
 class SMTPClient(object):
@@ -28,11 +30,12 @@ class SMTPClient(object):
 
         return self._session
 
-    def send_email(self, subject, text):
+    def send_email(self, subject, text, log_msg):
         """Sends email
 
         :param str subject: email subject
         :param str text: email text
+        :param str log_msg: shell tests logs
         """
 
         msg = MIMEMultipart()
@@ -42,18 +45,28 @@ class SMTPClient(object):
 
         msg.attach(MIMEText(text, 'plain'))
 
+        log_part = MIMEBase('application', 'octet-stream')
+        log_part.set_payload(log_msg)
+        Encoders.encode_base64(log_part)
+        log_part.add_header(
+            'Content-Disposition',
+            'attachment; filename="log.txt"',
+        )
+        msg.attach(log_part)
+
         return self.session.sendmail(self.user, self.recipients, msg.as_string())
 
     @staticmethod
     def _get_shell_name(shell_path):
         return re.split(r'[\\/]', shell_path)[-1]
 
-    def send_tests_result(self, success, result, shell_path):
+    def send_tests_result(self, success, result, shell_path, log_msg):
         """Send tests result
 
         :param bool success: was tests success or not
         :param str result: test result as a string
         :param str shell_path: path to the Shell
+        :param str log_msg: shell tests logs
         """
 
         shell_name = self._get_shell_name(shell_path)
@@ -61,17 +74,18 @@ class SMTPClient(object):
         subject = 'Test was {} for "{}"'.format(success_str, shell_name)
         text = '{}\n\nTests output:\n{}'.format(subject, result)
 
-        self.send_email(subject, text)
+        self.send_email(subject, text, log_msg)
 
-    def send_error(self, error_msg, shell_path):
+    def send_error(self, error_msg, shell_path, log_msg):
         """Send error message
 
         :param str error_msg:
         :param str shell_path: path to Shell
+        :param str log_msg: shell tests logs
         """
 
         shell_name = self._get_shell_name(shell_path)
         subject = 'Test failed with an error for "{}"'.format(shell_name)
         text = '{}\n\nError message:\n{}'.format(subject, error_msg)
 
-        self.send_email(subject, text)
+        self.send_email(subject, text, log_msg)
