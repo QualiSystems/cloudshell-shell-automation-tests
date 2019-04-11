e39ddb74fe600996f2015c3d2ba237a6a3ebb9fa
Kyrylo Maksymenko <saklar13@gmail.com>
2019-04-11 14:26:39 +0300

    delete CS if it doesn't start correctly

--- shell_tests/run_tests.py
+++ shell_tests/run_tests.py
@@ -65,7 +65,7 @@ class AutomatedTestsRunner(object):
                 error = None
                 return run_tests_inst
             finally:
-                if error and self.conf.do_conf.delete_cs:
+                if error or self.conf.do_conf.delete_cs:
                     self.do_handler.end_reservation()
 
         if not attempts and error:
