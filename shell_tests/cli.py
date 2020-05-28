ff58da9a4664207cd8f5e730bd09ebc73f7f4645
Kyrylo Maksymenko <saklar13@gmail.com>
2020-05-28 11:09:47 +0300

    update cli

--- shell_tests/cli.py
+++ shell_tests/cli.py
@@ -1,10 +1,11 @@
-import logging
+from pathlib import Path
 
 import click
 
 from shell_tests.configs import MainConfig
+from shell_tests.helpers.cli_helpers import PathPath
+from shell_tests.helpers.download_files_helper import DownloadFile
 from shell_tests.run_tests import AutomatedTestsRunner
-from shell_tests import oop_shellfoundry
 
 
 @click.group()
@@ -12,49 +13,18 @@ def cli():
     pass
 
 
-def get_logger():
-    log_level = logging.DEBUG
+@cli.command("run_tests")
+@click.argument("test_conf", type=PathPath(exists=True, dir_okay=False))
+def run_tests(test_conf: Path):
+    try:
+        conf = MainConfig.from_yaml(test_conf)
+        report = AutomatedTestsRunner(conf).run()
+    finally:
+        DownloadFile.remove_downloaded_files()
 
-    logger = logging.getLogger('Automation Tests')
-    logger.setLevel(log_level)
-
-    file_handler = logging.FileHandler('shell-tests.log', 'w')
-    file_handler.setLevel(log_level)
-    std_handler = logging.StreamHandler()
-    std_handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('%(asctime)s - %(threadName)s - %(levelname)s - %(message)s')
-    std_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(std_handler)
-    logger.addHandler(file_handler)
-
-    return logger
-
-
-@cli.command('run_tests')
-@click.argument('test_conf')
-@click.argument('env_conf', required=False)
-def run_tests(test_conf, env_conf=None):
-    logger = get_logger()
-    conf = MainConfig.parse_from_yaml(test_conf, env_conf)
-
-    report = AutomatedTestsRunner(conf, logger).run()
-
-    print('\n\nTest results:\n{}'.format(report))
+    print(f"\n\nTest results:\n{report}")  # noqa
     return report.is_success, report
 
 
-@cli.command('check_shellfoundry_templates')
-@click.argument('template_path')
-@click.argument('test_conf')
-def check_shellfoundry_templates(template_path, test_conf):
-    logger = get_logger()
-
-    oop_shellfoundry.check_shellfoundry_templates(logger, template_path, test_conf)
-
-
-if __name__ == '__main__':
-    import sys
-    cli(sys.argv[1:])
+if __name__ == "__main__":
+    cli()
