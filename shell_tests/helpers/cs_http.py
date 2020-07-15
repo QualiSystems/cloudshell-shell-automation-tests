013dde27b4521106df7dcf89596497e9267c2b8c
Kyrylo Maksymenko <saklar13@gmail.com>
2020-07-15 11:22:46 +0300

    fix getting reservation errors

--- shell_tests/helpers/cs_http.py
+++ shell_tests/helpers/cs_http.py
@@ -16,7 +16,9 @@ def get_reservation_errors(
     url = f"http://{conf.host}/"
     workspace_api = f"{url}api/WorkspaceApi/"
     login_url = f"{url}Account/Login"
-    get_activities_url = f"GetFilteredActivityFeedInfoList?diagramId={reservation_id}"
+    get_activities_url = (
+        f"{workspace_api}GetFilteredActivityFeedInfoList?diagramId={reservation_id}"
+    )
     public_key_url = f"{url}Account/PublicKey"
     get_activity_url = f"{workspace_api}GetActivityFeedInfo?eventId="
     data = {
@@ -28,9 +30,9 @@ def get_reservation_errors(
         resp = session.get(public_key_url)  # download public key
         public_key = serialization.load_pem_public_key(resp.content, default_backend())
 
-        enc_user = public_key.encrypt(conf.user, padding.PKCS1v15())
+        enc_user = public_key.encrypt(conf.user.encode(), padding.PKCS1v15())
         enc_user = base64.b64encode(enc_user)
-        enc_pass = public_key.encrypt(conf.password, padding.PKCS1v15())
+        enc_pass = public_key.encrypt(conf.password.encode(), padding.PKCS1v15())
         enc_pass = base64.b64encode(enc_pass)
 
         session.post(login_url, data={"username": enc_user, "password": enc_pass})
