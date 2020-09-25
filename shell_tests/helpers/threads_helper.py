0c867f605e92da033b373b86933d2c10cfc54ca8
Kyrylo Maksymenko <saklar13@gmail.com>
2020-09-25 13:32:20 +0300

    add ability to create networking apps without creating CS on Do

--- shell_tests/helpers/threads_helper.py
+++ shell_tests/helpers/threads_helper.py
@@ -1,5 +1,5 @@
 import time
-from threading import Thread
+from threading import Thread, current_thread
 from typing import List
 
 
@@ -7,3 +7,8 @@ def wait_for_end_threads(threads: List[Thread]):
     """Endless loop that wait for ending the threads."""
     while any(map(Thread.is_alive, threads)):
         time.sleep(1)
+
+
+def set_thread_name_with_prefix(suffix):
+    name = current_thread().name
+    current_thread().name = f"{name.rsplit('_', 1)[0]}-{suffix}"
